25e929a39bb6c81540c83f6187fda7350371addb
JakeFerguson007 <jake_ferguson@yahoo.com>
2026-08-12 12:47:16 -0500

    Add Mandarin lesson data

---
+++ data.js
@@ -0,0 +1,38 @@
+const lessons = [
+    {
+        lessonNumber: 1,
+        title: "First 5 Characters",
+        characters: [
+            { symbol: "我", pinyin: "wǒ", meaning: "I / me" },
+            { symbol: "你", pinyin: "nǐ", meaning: "you" },
+            { symbol: "他", pinyin: "tā", meaning: "he / him" },
+            { symbol: "人", pinyin: "rén", meaning: "person" },
+            { symbol: "是", pinyin: "shì", meaning: "to be / is" }
+        ]
+    },
+    {
+        lessonNumber: 2,
+        title: "Next 5 Characters",
+        characters: [
+            { symbol: "不", pinyin: "bù", meaning: "not / no" },
+            { symbol: "有", pinyin: "yǒu", meaning: "have / there is" },
+            { symbol: "在", pinyin: "zài", meaning: "at / in" },
+            { symbol: "大", pinyin: "dà", meaning: "big / large" },
+            { symbol: "小", pinyin: "xiǎo", meaning: "small" }
+        ]
+    }
+];
+
+const wordLessons = [
+    {
+        lessonNumber: 1,
+        title: "Words from Lessons 1–2",
+        words: [
+            { word: "我有", pinyin: "wǒ yǒu", meaning: "I have" },
+            { word: "你有", pinyin: "nǐ yǒu", meaning: "you have" },
+            { word: "大人", pinyin: "dàrén", meaning: "adult" },
+            { word: "不是", pinyin: "bú shì", meaning: "is not" },
+            { word: "你是", pinyin: "nǐ shì", meaning: "you are" }
+        ]
+    }
+];
